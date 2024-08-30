7a7c73632be9c20521525856282aa3819b9010b6
Bilal Uygur <bilalug930@gmail.com>
2024-08-30 11:40:20 +0300

    turtle bg color changed

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import turtle
 
 drawing_board = turtle.Screen()
-drawing_board.bgcolor("green")
+drawing_board.bgcolor("#CDBF1D")
 drawing_board.title("Python Turtle")
-turtle_instance = turtle.Turtle()
 
+turtle_instance = turtle.Turtle()
 turtle_instance.forward(100)
 turtle.done()
