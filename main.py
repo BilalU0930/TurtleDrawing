2dfc47f39e039b5b8ab54a7f8339a85478a57f0f
Bilal Uygur <bilalug930@gmail.com>
2024-08-30 11:56:23 +0300

    turtle drawing completed

--- main.py
+++ main.py
@@ -13,10 +13,33 @@ turtle_instance_2.forward(100)
 '''
 
 #square
+
+'''
 turtle_instance_3 = turtle.Turtle()
 
 for i in range(4):
+    turtle_instance_3.forward(50)
     turtle_instance_3.left(90)
-    turtle_instance_3.forward(100)
+'''
+
+#star
+'''
+turtle_instance = turtle.Turtle()
+
+for i in range(5):
+    turtle_instance.right(144)
+    turtle_instance.forward(100)
+'''
+
+
+#polygon
+turtle_instance = turtle.Turtle()
+num_sides = 6
+angle = 360.0 / num_sides
+side_length = 100
+
+for i in range(num_sides):
+    turtle_instance.right(angle)
+    turtle_instance.forward(side_length)
 
 turtle.done()
