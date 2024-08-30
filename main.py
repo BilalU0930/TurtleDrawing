c3c824daf265fd6fca04176d2cc87c205dda2c63
Bilal Uygur <bilalug930@gmail.com>
2024-08-30 16:19:29 +0300

    turtle drawing last finish

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import turtle
 
-'''
 drawing_board = turtle.Screen()
 drawing_board.bgcolor("light blue")
 drawing_board.title("Python Turtle")
 
+'''
 turtle_instance = turtle.Turtle()
 turtle_instance.forward(100)
 turtle_instance_2 = turtle.Turtle()
