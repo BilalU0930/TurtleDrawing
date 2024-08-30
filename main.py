88cbeca95225f6fa86612d6a5cb35c6da58e1cbd
Bilal Uygur <bilalug930@gmail.com>
2024-08-30 11:35:27 +0300

    turtle drawing completed

---
+++ main.py
@@ -0,0 +1,9 @@
+import turtle
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("green")
+drawing_board.title("Python Turtle")
+turtle_instance = turtle.Turtle()
+
+turtle_instance.forward(100)
+turtle.done()
