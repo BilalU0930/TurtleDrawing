31aa3e227daf0cc8cfa5e4487c5e2fdd27288bf5
Bilal Uygur <bilalug930@gmail.com>
2024-08-30 16:30:38 +0300

    turtle file finished

---
+++ shrinkingsquare.py
@@ -0,0 +1,25 @@
+import turtle
+
+turtle_screen = turtle.Screen()
+turtle_screen.bgcolor("light green")
+turtle_screen.title("Turtle Python")
+
+turtle_instance = turtle.Turtle()
+turtle_instance.color("blue")
+
+def shrinkingSquare(size):
+    for i in range(4):
+        turtle_instance.forward(size)
+        turtle_instance.left(90)
+        size = size - 5
+
+shrinkingSquare(150)
+shrinkingSquare(130)
+shrinkingSquare(110)
+shrinkingSquare(90)
+shrinkingSquare(70)
+shrinkingSquare(50)
+shrinkingSquare(30)
+shrinkingSquare(10)
+
+turtle.done()
