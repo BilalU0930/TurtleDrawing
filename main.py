cee7e0ffb853de216d130eacadfebd601209a7b4
Bilal Uygur <bilalug930@gmail.com>
2024-08-30 11:45:56 +0300

    turtle bg color changed and square drawing completed

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import turtle
 
 drawing_board = turtle.Screen()
-drawing_board.bgcolor("#CDBF1D")
+drawing_board.bgcolor("light blue")
 drawing_board.title("Python Turtle")
 
 turtle_instance = turtle.Turtle()
 turtle_instance.forward(100)
+turtle_instance_2 = turtle.Turtle()
+turtle_instance_2.left(180)
+turtle_instance_2.forward(100)
+
+#square
+
+turtle_instance_3 = turtle.Turtle()
+
+for i in range(4):
+    turtle_instance_3.left(90)
+    turtle_instance_3.forward(100)
+
 turtle.done()
